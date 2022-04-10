2880e6815b79e556f47ddcc9ad40e27592f14a0f
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-10 22:32:40 +0530

    uploading attendance file

---
+++ Checkattendance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeecomputation
+{
+    internal class Checkattendance
+    {
+        public static void attendance()
+        {
+            Random obj = new Random();
+            int num = obj.Next(0, 2);
+            if(num == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
